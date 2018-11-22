953ce637f7dcf912e358247615968af35af21930
Vlad Pokhytun <user@cdu.edu.ua>
2018-11-22 10:56:56 +0200

    Додано підрахунок суми чисел і виведення результату

--- Program1.cs
+++ Program1.cs
@@ -9,7 +9,9 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello world");
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("5 + 10 = {0}", x + y);
             Console.ReadKey();
         }
     }
